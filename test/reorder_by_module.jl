8d0e0c9b48c8901d39b6042d2e846618d452140e
Timothée Poisot <tim@poisotlab.io>
2015-09-07 22:45:51 -0400

    New tests for reorder_by_module

--- test/reorder_by_module.jl
+++ test/reorder_by_module.jl
@@ -2,6 +2,8 @@ module TestReorderByModule
   using Base.Test
   using Brim
 
+  # Test 1 - simple case
+
   A = [1 0 0; 0 0 1; 0 1 0]
   S = [0 1 0; 1 0 0; 0 0 1; 0 1 0; 0 0 1; 0 0 1]
   C = [1 0 0; 0 1 0; 0 0 1; 0 1 0; 0 0 1; 0 0 1]
@@ -10,5 +12,15 @@ module TestReorderByModule
 
   reorder_by_module!(M)
   @test sum(abs(M.S .- C)) == 0
+
+  # Test 2 - more complex case
+
+  A = [0 0 0 1 1 1; 1 1 1 0 0 0; 0 0 0 1 1 1; 1 1 1 0 0 0; 0 0 0 1 1 1; 1 1 1 0 0 0]
+  M = recursive_brim!(partition_random(A))
+  reorder_by_module!(M)
+
+  target = [1 0; 1 0; 1 0; 0 1; 0 1; 0 1; 1 0; 1 0; 1 0; 0 1; 0 1; 0 1]
+
+  @test hash(M.S) == hash(target)
 
 end
